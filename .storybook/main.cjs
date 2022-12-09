d96a2e8a2f81915509f74bb21f815a9eaeeffff6
julio <Julio@zoocha.com>
2022-12-08 21:08:43 -0300

    upgrade next

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,32 +1,25 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
-    '@storybook/addon-interactions'
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", '@storybook/addon-a11y', '@storybook/addon-interactions'],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
+  "core": {},
   "features": {
     "storyStoreV7": true,
     " interactionsDebugger": true
   },
-  "staticDirs": [
-    "../public"
-  ],
-  viteFinal: (config, { configType }) => {
+  "staticDirs": ["../public"],
+  viteFinal: (config, {
+    configType
+  }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/react-storybook-figma/'
+      config.base = '/react-storybook-figma/';
     }
-
-    return config
+    return config;
+  },
+  docs: {
+    docsPage: "automatic"
   }
-
-}
+};
